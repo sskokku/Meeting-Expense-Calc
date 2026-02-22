c694020dd9269abda841be21b597a4957f92a6c5
Sashidhar Kokku <sashidhar.kokku@gmail.com>
2026-02-22 12:16:33 -0500

    Initial Commit

---
+++ Meeting Expense Calc/Meeting_Expense_CalcApp.swift
@@ -0,0 +1,17 @@
+//
+//  Meeting_Expense_CalcApp.swift
+//  Meeting Expense Calc
+//
+//  Created by Sashidhar Kokku on 2/22/26.
+//
+
+import SwiftUI
+
+@main
+struct Meeting_Expense_CalcApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Meeting Expense CalcTests/Meeting_Expense_CalcTests.swift
@@ -0,0 +1,17 @@
+//
+//  Meeting_Expense_CalcTests.swift
+//  Meeting Expense CalcTests
+//
+//  Created by Sashidhar Kokku on 2/22/26.
+//
+
+import Testing
+@testable import Meeting_Expense_Calc
+
+struct Meeting_Expense_CalcTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
